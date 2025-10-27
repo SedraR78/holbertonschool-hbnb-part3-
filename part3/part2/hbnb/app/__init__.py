450f3a34d06e07ad0db6434f26e88b3ab4f831d5
Sedra Ramarosaona <sedranante@gmail.com>
2025-10-27 13:47:42 +0100

    feat(config) : starting configuring my project

--- part3/part2/hbnb/app/__init__.py
+++ part3/part2/hbnb/app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_restx import Api, Resource
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 from .api.v1.reviews import api as reviews_ns 
 from .api.v1.places import api as places_ns 
 from .api.v1.amenities import api as amenities_ns
 from .api.v1.users import api as users_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    bcrypt.init_app(app)
 
  
     # Register all API namespaces
--- part3/part2/hbnb/app/models/user.py
+++ part3/part2/hbnb/app/models/user.py
@@ -24,3 +24,11 @@ class User(BaseModel):
         domaine = self.email.split("@")[-1]  #
         if "." not in domaine:
             raise ValueError("Email must contain domain with .")
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
