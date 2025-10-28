57c055f6f6937351e94b8306a85cdec4fba18bc9
ValentinDLC <valentin.dardenne9@gmail.com>
2025-10-28 18:17:04 +0100

    Main config setup with env variables

--- part3/part2/config.py
+++ part3/part2/config.py
@@ -1,13 +1,43 @@
 import os
+from dotenv import load_dotenv
+
+# Charge les variables d'environnement depuis le fichier .env
+load_dotenv()
+
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
     DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///hbnb.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    API_TITLE = "HBnB API"
+    API_VERSION = "1.0"
+    API_DESCRIPTION = "API REST de l'application HBnB"
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    ENV = "development"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DATABASE_URL", "sqlite:///dev.db")
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    ENV = "testing"
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL", "sqlite:///test.db")
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ENV = "production"
+    SQLALCHEMY_DATABASE_URI = os.getenv("PROD_DATABASE_URL", "sqlite:///prod.db")
+
 
 config = {
-    'development': DevelopmentConfig,
-    'default': DevelopmentConfig
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig,
 }
