1ec083536beb2e2dbd849437b30dbbb4bca4062e
ValentinDLC <valentin.dardenne9@gmail.com>
2025-10-28 18:20:16 +0100

    API and root

--- part3/part2/hbnb/app/__init__.py
+++ part3/part2/hbnb/app/__init__.py
@@ -1,31 +1,41 @@
 from flask import Flask
 from flask_restx import Api, Resource
-
-from .api.v1.reviews import api as reviews_ns 
-from .api.v1.places import api as places_ns 
+from config import config
+from .api.v1.reviews import api as reviews_ns
+from .api.v1.places import api as places_ns
 from .api.v1.amenities import api as amenities_ns
 from .api.v1.users import api as users_ns
 
-def create_app():
+
+def create_app(config_name="default"):
+    """Create Flask app and register API namespaces based on config."""
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    app.config.from_object(config[config_name])
 
- 
-    # Register all API namespaces
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(users_ns, path='/api/v1/users')
+    api = Api(
+        app,
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API",
+        doc="/api/v1/",  # Swagger UI at /api/v1/
+    )
 
-    @api.route('/')
+    # Register API namespaces
+    api.add_namespace(places_ns, path="/api/v1/places")
+    api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    api.add_namespace(users_ns, path="/api/v1/users")
+
+    # Simple root endpoint
+    @api.route("/")
     class WelcomeResource(Resource):
         def get(self):
             return {"message": "Welcome to HBNB API - Visit /api/v1/ for documentation"}
-    
-    # Hello endpoint (tu peux garder si tu veux)
-    @api.route('/hello')
+
+    # Another simple test endpoint
+    @api.route("/hello")
     class HelloResource(Resource):
         def get(self):
             return {"message": "Hello from HBNB API"}
-    
+
     return app
