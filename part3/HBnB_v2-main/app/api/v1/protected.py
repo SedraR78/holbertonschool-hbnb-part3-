c6754c6aeb4ee258a766f119ead7604d2c927a1d
Sedra Ramarosaona <sedranante@gmail.com>
2025-10-29 14:37:29 +0100

    Modify the User Model to Include Password Hashing

---
+++ part3/HBnB_v2-main/app/api/v1/protected.py
@@ -0,0 +1,19 @@
+from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+
+api = Namespace('protected', description='Protected endpoints')
+
+@api.route('/protected')
+class ProtectedResource(Resource):
+    @jwt_required()
+    def get(self):
+        """A protected endpoint that requires a valid JWT token"""
+        current_user = get_jwt_identity()  # Retrieve the user's identity from the token
+        
+        # If you need to see if the user is an admin or not, you can access additional claims using get_jwt()
+        claims = get_jwt()
+        
+        return {
+            'message': f'Hello, user {current_user}',
+            'is_admin': claims.get('is_admin', False)
+        }, 200
